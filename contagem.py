3669f3c4f0a073cd0f1e6c7b304515d6d0bda407
unknown <wellington silva>
2026-02-23 13:08:37 -0300

    Envio do projeto

---
+++ contagem.py
@@ -0,0 +1,5 @@
+print("Contagem regressiva iniciada!")
+
+for numero in range(10, 0, -1):
+    print(numero)
+print("Contagem regressiva concluída!")
--- escala.py
+++ escala.py
@@ -1,5 +1,13 @@
 nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("digite a segunda nota:"))
 
+media = (nota1 + nota2) / 2
 
+print(f"\nSua média final foi {media}")
 
-
+if media >= 7.0:
+    print("Resultado: Aprovado(a)!")
+elif media >= 5.0:
+    print("Resultado: RECUPERAÇÃO . Estude mais e tente novamente!")
+else: 
+    print("Resultado: REPROVADO(a). Não desista, continue se esforçando e buscando melhorar!")
